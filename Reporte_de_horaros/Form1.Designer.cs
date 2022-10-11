c81231efab8ea316b94d9c5c0280a04a1e3b12b7
Carlos-pixel24 <carlos.utn.es@gmail.com>
2022-10-11 13:54:29 -0700

    Revert "prueba 2"
    This reverts commit b2bd8713739dc5146c6e9f73f952635f6da82a38.

--- Reporte_de_horaros/Form1.Designer.cs
+++ Reporte_de_horaros/Form1.Designer.cs
@@ -28,32 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(266, 189);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Prueba";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form1";
             this.Text = "Form1";
-            this.ResumeLayout(false);
-
         }
 
         #endregion
-
-        private Button button1;
     }
 }
